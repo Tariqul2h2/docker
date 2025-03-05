3f838a3dbfa5f157e236ee28a7e9ef76a327c7ad
tariq <tariqul.rabby@gmail.com>
2025-03-06 03:28:00 +0600

    Shell updated

--- docker_install.sh
+++ docker_install.sh
@@ -19,10 +19,7 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyring
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add Docker's repository to APT sources
-echo \  
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \  
-  $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | \  
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update the package index again to include Docker's repository
 sudo apt-get update
